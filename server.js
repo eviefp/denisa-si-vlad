c099151069439b4d202d3b3c8b9e9b1077798a74
Vladimir Ciobanu <v.ciobanu@moodmedia.com>
2016-06-12 23:33:04 +0300

    no message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('source/public'));
+app.use(express.static('public'));
 
 app.listen(process.env.port || 4000, function() {
     console.log('Running on port 4000');
